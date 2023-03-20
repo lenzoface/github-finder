4224518852698235f9897f98de000deda0b0433b
Sherkhan K <sherkhan4work@gmail.com>
2023-03-20 15:16:41 +0600

    useContext setup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
+    <GithubProvider>
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
@@ -19,9 +21,11 @@ function App() {
             <Route path='/*' element={<NotFound />} />
           </Routes>
         </main>
+        
         <Footer />
       </div>
     </Router>
+    </GithubProvider>
   );
 }
 

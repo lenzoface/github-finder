eb73819c3259eb4e7d46c618e9fca2cf20fa00ab
Sherkhan K <sherkhan4work@gmail.com>
2023-03-23 11:54:35 +0600

    Alert above searchbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
         <main className="container mx-auto px-3 pb-12">
+          <Alert />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
@@ -25,6 +29,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }

2b5ab38ddfe091e9a5388aaf8b5f58608be92f03
Sherkhan K <sherkhan4work@gmail.com>
2023-03-25 11:59:45 +0600

    Slight styles change

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -14,7 +14,7 @@ function RepoItem({repo}) {
         stargazers_count,
     } = repo
 
-  return <div className='mb-2 rounded-md card bg-gray-800 hover:bg-gray-900'>
+  return <div className='mb-2 rounded-md card bg-neutral hover:bg-accent-content'>
     <div className="card-body text-white">
         <h3 className="mb-2 text-xl font-semibold">
             <a href={html_url}> 
